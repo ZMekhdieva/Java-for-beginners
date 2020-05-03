d035d7e5b5d88317830a0fb9d8d9cc138a05dbfc
ZMekhdieva <jannaeremeeva@gmail.com>
2020-05-03 20:42:58 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input number: ");
+        int num = in.nextInt();
+        System.out.println(num + " * 1 = " + num + "\n"
+                            + num + " * 2 = " + num*2 + "\n"
+                            + num + " * 3 = " + num*3 + "\n"
+                            + num + " * 4 = " + num*4 + "\n"
+                            + num + " * 5 = " + num*5 + "\n"
+                            + num + " * 6 = " + num*6 + "\n"
+                            + num + " * 7 = " + num*7 + "\n"
+                            + num + " * 8 = " + num*8 + "\n"
+                            + num + " * 9 = " + num*9 + "\n"
+                            + num + " * 10 = " + num*10 + "\n");
+
+           }
+}
