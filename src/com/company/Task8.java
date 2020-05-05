2d9b9344977fed6c86229af68ed9677584ed3604
ZMekhdieva <jannaeremeeva@gmail.com>
2020-05-05 19:19:05 +0300

    Task8 added

---
+++ src/com/company/Task8.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Task8 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 50; i++) {
+            System.out.print(i + " ");
+            System.out.print("\n");
+        }
+    }
+}
