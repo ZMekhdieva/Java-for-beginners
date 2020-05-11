f870f2be916002ff1d01fa4e04e457318b61ecb6
ZMekhdieva <jannaeremeeva@gmail.com>
2020-05-11 14:33:42 +0300

    Task11 added

---
+++ src/com/company/Task11.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.Scanner;
+public class Task11 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите строку: ");
+        String s = in.nextLine();
+        System.out.println(s);
+    }
+}
