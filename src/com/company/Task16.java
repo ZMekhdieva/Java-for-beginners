ce70e59c30d1a359101d6a2e35b61d98775f281f
ZMekhdieva <jannaeremeeva@gmail.com>
2020-05-11 21:33:10 +0300

    Task16 added

--- src/com/company/Task16.java
+++ src/com/company/Task16.java
@@ -1,4 +1,19 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
 public class Task16 {
+    public static void main(String[] args) {
+        String s;
+        try (BufferedReader br = new BufferedReader(new FileReader("Text.txt"))) {
+            while ((s = br.readLine()) != null) {
+                System.out.println(s);
+            }
+        } catch (IOException exc) {
+            System.out.println("Ощибка ввода-вывода: " + exc);
+        }
+    }
 }
